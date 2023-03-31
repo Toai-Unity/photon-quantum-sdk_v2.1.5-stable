142eb3dc620c4266fe929916e5d89a523cc2efc4
Toai-Unity <toainv@wolffungame.com>
2023-03-31 17:57:57 +0700

    them mot ti code score

--- quantum_code/quantum.code/Assets/StatusData.cs
+++ quantum_code/quantum.code/Assets/StatusData.cs
@@ -1,8 +1,12 @@
 ﻿
 
+using Photon.Deterministic;
+
 namespace Quantum
 {
     public partial class StatusData
     {
+        public FP MaxHealth;
+        public FP TimeToRespawn;
     }
 }
---
+++ quantum_code/quantum.code/Systems/StatusSystem.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Photon.Deterministic;
+
+namespace Quantum
+{
+    public unsafe class StatusSystem : SystemMainThread
+    {
+        public override void Update(Frame f)
+        {
+        }
+
+        private static void TakeDamage(Frame frame, EntityRef sourceEnemyDamage, EntityRef tank, FP damage)
+        {
+            Status* tankStatusComp = frame.Unsafe.GetPointer<Status>(tank);
+            StatusData statusData = frame.FindAsset<StatusData>(tankStatusComp->StatusData.Id);
+
+            // Do something to take damage
+
+            // Check if tank die
+            if (tankStatusComp-> CurrentHealth <= 0)
+            {
+                KillRobot(frame, sourceEnemyDamage, tank, statusData.TimeToRespawn);
+            }
+
+        }
+
+        private static void KillRobot(Frame frame, EntityRef killer, EntityRef robot, FP respawnTime)
+        {
+
+        }
+    }
+}
